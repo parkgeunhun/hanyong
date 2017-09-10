002ecf2cd86dbc39e9f7b1f4ab25526e20110f11
leehanyong <god4baby@naver.com>
2017-09-10 21:48:36 +0900

    first

---
+++ ex1-chap01/src/main/java/com/javalec/ex/BuildRunner.java
@@ -0,0 +1,9 @@
+package com.javalec.ex;
+
+import java.util.List;
+
+public interface BuildRunner {
+
+	public void build(List<String> srcDirs, String binDir);
+
+}
---
+++ ex1-chap01/src/main/java/com/javalec/ex/MainBySpring.java
@@ -0,0 +1,16 @@
+package com.javalec.ex;
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class MainBySpring {
+
+	public static void main(String[] args) {
+		String configLocation = "classpath:applicationContext.xml";
+		AbstractApplicationContext ctx = 
+				new GenericXmlApplicationContext(configLocation);
+		Project project = ctx.getBean("sampleProject", Project.class);
+		project.build();
+		ctx.close();
+	}
+}
